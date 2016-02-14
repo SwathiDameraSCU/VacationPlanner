c35dc0045b513447b159f12c5217f7be62fb14df
Kanika Agrawal <kanikaagrawal@Kanikas-MacBook-Pro.local>
2016-02-14 12:00:29 -0800

    server initial commit

---
+++ vacation_planner_server/app/controllers/Airport.java
@@ -0,0 +1,17 @@
+package controllers;
+
+/**
+ * Created by kanikaagrawal on 2/6/16.
+ */
+public class Airport
+{
+    public String code;
+    public String name;
+    public String city;
+    public String country;
+    public String timezone;
+    public Float lat;
+    public Float lng;
+    public String terminal;
+    public String gate;
+}
---
+++ vacation_planner_server/app/controllers/FlightDetails.java
@@ -0,0 +1,15 @@
+package controllers;
+
+/**
+ * Created by kanikaagrawal on 2/6/16.
+ */
+public class FlightDetails
+{
+    String departureTime;
+    String arrivalTime;
+    Integer duration;
+    String destination;
+    String origin;
+    Integer connectionDuration;
+    String flightId;
+}
