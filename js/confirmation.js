625e3ef7571a4adbc105d830f357fa89b0133aa7
sumanahariharan <sumanahariharan@gmail.com>
2016-03-09 14:52:25 -0800

    contact us css , confirmation page text message added

--- js/confirmation.js
+++ js/confirmation.js
@@ -5,7 +5,7 @@ $( window ).load(function() {
       if (booking_id == "") {
         $("#message-div").html("Your ticket was not booked.Please try again later");
       } else {
-        $("#message-div").html("Your ticket has been booked successfully and your <b> booking id is " + booking_id + "</b>");
+        $("#message-div").html("Your ticket has been booked successfully and your <b> booking id is " + booking_id + "</b>." + "<br> Confirmation email sent to <b>" + localStorage.getItem("emailId") + "</b>.");
       }
     } else {
       alert("Sorry, your browser does not support Web Storage...");
